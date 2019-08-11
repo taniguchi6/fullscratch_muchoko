575d163c459ef3539f8d31ecf3d8bcb55dc1b6cf
谷口 <semikoron.koron@gmail.com>
2019-08-12 01:49:41 +0900

    update:画像表示処理作成途中

--- main.php
+++ main.php
@@ -19,10 +19,11 @@
     echo '<br>';
     $id = $_SESSION['id'];
 
-    if($_SERVER['REQUEST_METHOD'] != 'POST') {
-      //画像を取得
+    if(isset($_POST['submit']) && !empty($_POST['post'])) {
+      $text_post = $_POST['post'];
+      $user_id = $_SESSION['id'];
+      var_dump($text_post);
 
-    }else {
       //画像を保存
       if(!empty($_FILES['image']['name'])) {
         $image_name = $_FILES['image']['name'];
@@ -30,20 +31,15 @@
         $image_content = file_get_contents($_FILES['image']['tmp_name']);
         $image_size = $_FILES['image']['size'];
 
-        $sql = 'INSERT INTO images(image_name, image_type, image_content, image_size, created_at) VALUES (:image_name, :image_type, :image_content, :image_size, now())';
+        $sql = 'INSERT INTO images(image_name, image_type, image_content, image_size, created_at, image_user_id) VALUES (:image_name, :image_type, :image_content, :image_size, now(), :image_user_id)';
         $prepare = $dbh->prepare($sql);
         $prepare->bindValue(':image_name', $image_name, PDO::PARAM_STR);
         $prepare->bindValue(':image_type', $image_type, PDO::PARAM_STR);
         $prepare->bindValue(':image_content', $image_content, PDO::PARAM_STR);
         $prepare->bindValue(':image_size', $image_size, PDO::PARAM_INT);
+        $prepare->bindValue(':image_user_id', $user_id, PDO::PARAM_INT);
         $prepare->execute();
       }
-    }
-
-    if(isset($_POST['submit']) && !empty($_POST['post'])) {
-      $text_post = $_POST['post'];
-      $user_id = $_SESSION['id'];
-      var_dump($text_post);
 
       $prepare = $dbh->prepare("INSERT INTO text_posts (post_id, user_id, text, date) VALUES (NULL, :user_id, :text_post, CURRENT_TIMESTAMP)");
       $prepare->bindValue(':user_id', (int)$user_id, PDO::PARAM_INT);
@@ -62,11 +58,11 @@
     }
 
     // 全ての投稿を取得
-    $prepare = $dbh->prepare('SELECT * FROM users, text_posts WHERE users.id = text_posts.user_id');
+    $prepare = $dbh->prepare('SELECT * FROM users, text_posts, images WHERE users.id = text_posts.user_id AND users.id = images.image_user_id');
     $prepare->execute();
 
     $text_posts = $prepare->fetchAll();
-    print_r($text_posts);
+    //print_r($text_posts);
     echo '<br>';
 
   } catch (PDOException $e) {
@@ -86,15 +82,12 @@
 <body>
   <h2>ようこそ<?php echo $_SESSION['username'];?>さん</h2>
   <div>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-      <label for="post">投稿内容</label>
-      <input name="post" id="post" cols="30" rows="10">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post" enctype="multipart/form-data">
+      <p><label for="post">投稿内容</label></p>
+      <textarea name="post" id="post" cols="30" rows="10"></textarea>
+      <p><label for="image">画像をアップロード</label></p>
+      <input id="image" type="file" name="image" accept="image/png,image/jpeg">
       <button type="submit" name="submit">投稿</button>
-    </form>
-
-    <form method="post" action="<?php print basename( __FILE__ ); ?>" enctype="multipart/form-data">
-      <input type="file" name="image" accept="image/png,image/jpeg">
-      <input type="submit" value="画像アップロード">
     </form>
   </div>
   <div>
@@ -113,11 +106,19 @@
         <p class="time">
           <?php echo $text_post['date']; ?>
         </p>
+        <?php if($text_post['image_id']): ?>
+          <?php
+            $DB_PIC = $text_post['image_content'];
+
+            $finfo    = finfo_open(FILEINFO_MIME_TYPE);
+            $mimeType = finfo_buffer($finfo, $DB_PIC);
+            finfo_close($finfo);
+          ?>
+        <?php endif; ?>
         <?php if($text_post['user_id'] == $_SESSION['id']): ?>
           <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
             <input type="hidden" name="delete_id" value="<?php echo $text_post['post_id']; ?>">
-            <input type="submit" value="削除" name="delete_submit">
-          <a href="">投稿を削除</a>
+            <input type="submit" value="投稿を削除" name="delete_submit">
         <?php endif; ?>
       </div>
     <?php endforeach; ?>
