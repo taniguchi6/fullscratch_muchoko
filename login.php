3fddd7ccef7e9e368baad0ecdc470d1e02cf3daf
谷口 <semikoron.koron@gmail.com>
2019-08-11 09:56:55 +0900

    add:ログイン、ログアウト画面作成

---
+++ login.php
@@ -0,0 +1,47 @@
+<?php
+ //ob_start();
+ session_start();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>login</title>
+</head>
+<body>
+  <h2>ログイン画面</h2>
+  <div>
+  <?php
+    $msg = '';
+
+    if(isset($_POST['login']) && !empty($_POST['username']) && !empty($_POST['password'])) {
+      
+      if($_POST['username'] == 'tutorialspoint' && $_POST['password'] == '1234') {
+        $_SESSION['valid'] = true;
+        $_SESSION['timeout'] = time();
+        $_SESSION['username'] = 'tutorialspoint';
+
+        $msg = 'You have entered valid use name and password';
+      }else {
+        $msg = 'Wrong username or password';
+      }
+    }
+  ?>
+  </div>
+  <div>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+      <p><?php echo $msg; ?></p>
+      <p><?php var_dump($_POST['username']);?></p>
+      <p><?php var_dump($msg);?></p>
+      <p><?php var_dump($_POST['valid']);?></p>
+      <input type="text" name="username" placeholder="username = tutorialspoint" required autofocus><br>
+      <input type="password" placeholder="password = 1234" required>
+      <button type="submit" name="login">Login</button>
+    </form>
+
+    <P>Click here to clean <a href="logout.php" title="Logout">Session.</a></p>
+  </div>
+</body>
+</html>
