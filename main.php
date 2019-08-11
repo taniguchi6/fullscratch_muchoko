8c5db1c6524a3757d4a03b24fc6eecd2c6b4c3b5
谷口 <semikoron.koron@gmail.com>
2019-08-12 00:32:03 +0900

    update:削除処理追加

--- main.php
+++ main.php
@@ -30,18 +30,21 @@
       $user_id = $_SESSION['id'];
       var_dump($text_post);
 
-      $prepare = $dbh->prepare("INSERT INTO text_posts (id, user_id, text, date) VALUES (NULL, :user_id, :text_post, CURRENT_TIMESTAMP)");
+      $prepare = $dbh->prepare("INSERT INTO text_posts (post_id, user_id, text, date) VALUES (NULL, :user_id, :text_post, CURRENT_TIMESTAMP)");
       $prepare->bindValue(':user_id', (int)$user_id, PDO::PARAM_INT);
       $prepare->bindValue(':text_post', $text_post, PDO::PARAM_STR);
       $prepare->execute();
     }
-    // $prepare = $dbh->prepare('SELECT * FROM users, text_posts WHERE users.id = text_posts.user_id AND users.id = ?');
-    // $prepare->bindValue(1,(int)$id,PDO::PARAM_INT);
-    // $prepare->execute();
 
-    // $result = $prepare->fetch();
-    // print_r($result);
-    // echo '<br>';
+    // 削除処理
+    if(isset($_POST['delete_submit']) && !empty($_POST['delete_id'])) {
+      $delete_id = $_POST['delete_id'];
+      var_dump($delete_id);
+
+      $prepare = $dbh->prepare("DELETE FROM text_posts WHERE text_posts.post_id = ?");
+      $prepare->bindValue(1, (int)$delete_id, PDO::PARAM_INT);
+      $prepare->execute();
+    }
 
     // 全ての投稿を取得
     $prepare = $dbh->prepare('SELECT * FROM users, text_posts WHERE users.id = text_posts.user_id');
@@ -91,12 +94,16 @@
           <?php echo $text_post['date']; ?>
         </p>
         <?php if($text_post['user_id'] == $_SESSION['id']): ?>
+          <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+            <input type="hidden" name="delete_id" value="<?php echo $text_post['post_id']; ?>">
+            <input type="submit" value="削除" name="delete_submit">
           <a href="">投稿を削除</a>
         <?php endif; ?>
       </div>
     <?php endforeach; ?>
   </div>
 </body>
+
 <style>
 .post {
   border: 2px solid #efacde;
